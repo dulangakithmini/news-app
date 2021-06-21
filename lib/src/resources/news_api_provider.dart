56afd1753205285c780e46d36c2eb6d9fadd3615
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-21 06:03:20 +0530

    Create NewsApiProvider class and create an instance of the Client class inside of it

---
+++ lib/src/resources/news_api_provider.dart
@@ -0,0 +1,12 @@
+import 'package:http/http.dart' show Client;
+
+class NewsApiProvider {
+  /// Create an instance of the Client class
+  Client client = Client();
+
+  /// To get list of top Ids
+  fetchTopIds() {}
+
+  /// To get an item
+  fetchItem() {}
+}
