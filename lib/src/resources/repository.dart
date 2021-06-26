63a543c319bbd88c3c7d8ea7132453faaeab4354
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-27 01:47:09 +0530

    Create fetchTopIds function in Repository class

--- lib/src/resources/repository.dart
+++ lib/src/resources/repository.dart
@@ -1,8 +1,15 @@
 import 'news_api_provider.dart';
 import 'news_db_provider.dart';
 
+/// Govern access to either the NewsApiProvider or NewsDbProvider
 class Repository {
   /// Create instances of NewsApiProvider and NewsDbProvider
   NewsApiProvider apiProvider = NewsApiProvider();
   NewsDbProvider dbProvider = NewsDbProvider();
+
+  fetchTopIds() {
+    /// Fetch top ids from the NewsApiProvider.
+    /// We don't have access to top ids in NewsDbProvider
+    return apiProvider.fetchTopIds();
+  }
 }
