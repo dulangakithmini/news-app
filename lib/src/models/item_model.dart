756cdf8594ecb267184d7e86d416c66a7b4e9907
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-27 01:04:52 +0530

    Pass the fetched item to ItemModel.fromDb and return it

--- lib/src/models/item_model.dart
+++ lib/src/models/item_model.dart
@@ -15,19 +15,21 @@ class ItemModel {
   String title;
   int descendants;
 
-  ItemModel(this.id,
-      this.deleted,
-      this.type,
-      this.by,
-      this.time,
-      this.text,
-      this.dead,
-      this.parent,
-      this.kids,
-      this.url,
-      this.score,
-      this.title,
-      this.descendants,);
+  ItemModel(
+    this.id,
+    this.deleted,
+    this.type,
+    this.by,
+    this.time,
+    this.text,
+    this.dead,
+    this.parent,
+    this.kids,
+    this.url,
+    this.score,
+    this.title,
+    this.descendants,
+  );
 
   ItemModel.fromJson(Map<String, dynamic> parsedJson) {
     id = parsedJson['id'];
@@ -47,13 +49,19 @@ class ItemModel {
 
   ItemModel.fromDb(Map<String, dynamic> parsedJson) {
     id = parsedJson['id'];
+
+    /// Convert the integer value from DB to boolean
     deleted = parsedJson['deleted'] == 1;
     type = parsedJson['type'];
     by = parsedJson['by'];
     time = parsedJson['time'];
     text = parsedJson['text'];
+
+    /// Convert the integer value from DB to boolean
     dead = parsedJson['dead'] == 1;
     parent = parsedJson['parent'];
+
+    /// Take list of integers as a String and turn it into actual list of integers.
     kids = JsonDecoder(parsedJson['kids']) as List;
     url = parsedJson['url'];
     score = parsedJson['score'];
--- lib/src/resources/news_db_provider.dart
+++ lib/src/resources/news_db_provider.dart
@@ -5,6 +5,8 @@ import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
+import '../models/item_model.dart';
+
 class NewsDbProvider {
   /// Database type is from the sqflite package
   /// This instance variable refers the connection to the actual database in the device
@@ -55,5 +57,11 @@ class NewsDbProvider {
       where: "id = ?",
       whereArgs: [id],
     );
+
+    /// If there are any records with the given id
+    if (maps.length > 0) {
+      return ItemModel.fromDb(maps.first);
+    }
+    return null;
   }
 }
