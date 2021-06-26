63915641df23578fef440139e5e641a629d0a293
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-26 21:24:18 +0530

    Create a reference to a directory in the mobile device and join it with items.db

--- lib/src/resources/news_db_provider.dart
+++ lib/src/resources/news_db_provider.dart
@@ -1,7 +1,20 @@
+/// Allows working with device's file system
+import 'dart:io';
+
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
 class NewsDbProvider {
   /// Database type is from the sqflite package
   /// This instance variable refers the connection to the actual database in the device
   Database db;
+
+  init() async {
+    /// Directory from dart:io package and getApplicationDocumentsDirectory from path_provider
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+
+    /// From path package
+    final path = join(documentsDirectory.path, "items.db");
+  }
 }
