38834118e9761af9bf1ab03523ed549a02c44ac0
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-27 22:31:59 +0530

    Create NewsList Widget

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:news/src/screens/news_list.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('News'),
-        ),
-      ),
+      home: NewsList(),
     );
   }
 }
