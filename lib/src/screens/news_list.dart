3201bbc8318ec375631ac5059d5b21704526a178
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-29 02:37:44 +0530

    Create buildList method in NewsList

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -1,37 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:news/src/blocs/stories_bloc.dart';
+import 'package:news/src/blocs/stories_provider.dart';
 
 class NewsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final bloc = StoriesProvider.of(context);
+
+    /// bad practice
+    bloc.fetchTopIds();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Top News'),
       ),
-      body: buildList(),
+      body: buildList(bloc),
     );
   }
 
-  Widget buildList() {
-    return ListView.builder(
-      itemCount: 1000,
-      itemBuilder: (context, int index) {
-        return FutureBuilder(
-          future: getFuture(),
-          builder: (context, snapshot) {
-            return snapshot.hasData
-                ? Text('I\'m visible $index')
-                : Text('You can\'t see me');
+  Widget buildList(StoriesBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.topIds,
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Text('No data');
+        }
+        return ListView.builder(
+          /// Total number of ids fetched
+          itemCount: snapshot.data.length,
+
+          itemBuilder: (context, int index) {
+            return Text(snapshot.data[index]);
           },
         );
       },
     );
   }
 
+// Widget buildList(StoriesBloc bloc) {
+//   return ListView.builder(
+//     itemCount: 1000,
+//     itemBuilder: (context, int index) {
+//       return FutureBuilder(
+//         future: getFuture(),
+//         builder: (context, snapshot) {
+//           return snapshot.hasData
+//               ? bloc.topIds
+//               : Text('You can\'t see me');
+//         },
+//       );
+//     },
+//   );
+// }
+
   /// Fake Future to be used in future of the FutureBuilder
-  getFuture() {
-    return Future.delayed(
-      Duration(seconds: 2),
-      () => 'Hi',
-    );
-  }
+// getFuture() {
+//   return Future.delayed(
+//     Duration(seconds: 2),
+//     () => 'Hi',
+//   );
+// }
 }
