f3a46844f39927416f3c4d419890ef3c7aad56f8
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-29 01:57:14 +0530

    Create fetchTopIds method in StoriesBloc class

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -1,10 +1,21 @@
+import 'package:news/src/resources/repository.dart';
 import 'package:rxdart/rxdart.dart';
 
 class StoriesBloc {
   final _topIds = PublishSubject<List<int>>();
 
+  final _repository = Repository();
+
   /// Getters to streams
   Stream<List<int>> get topIds => _topIds.stream;
+
+  fetchTopIds() async {
+    /// Fetch Ids from the repository
+    final ids = await _repository.fetchTopIds();
+
+    /// Add to the PublishSubject
+    _topIds.sink.add(ids);
+  }
 
   dispose() {
     _topIds.close();
