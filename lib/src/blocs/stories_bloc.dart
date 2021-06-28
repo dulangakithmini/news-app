a74c7f4d81bec53c29e152ba1750b96a01919a12
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-29 01:39:56 +0530

    Create a PublishSubject in StoriesBloc

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -1,1 +1,12 @@
-class StoriesBloc {}
+import 'package:rxdart/rxdart.dart';
+
+class StoriesBloc {
+  final _topIds = PublishSubject<List<int>>();
+
+  /// Getters to streams
+  Stream<List<int>> get topIds => _topIds.stream;
+
+  dispose() {
+    _topIds.close();
+  }
+}
