eb23272d0ae783c6476ab84b0a835000a12073e8
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-29 01:59:15 +0530

    Wrap MaterialApp with StoriesProvider

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news/src/blocs/stories_provider.dart';
 import 'package:news/src/screens/home_screen/blocs/home_bloc.dart';
-
 // import 'package:news/src/screens/home_screen/home_screen.dart';
 import 'package:news/src/screens/news_list.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: BlocProvider<HomeBloc>(
-        create: (ctx) => HomeBloc(),
-        child: NewsList(),
+    return StoriesProvider(
+      child: MaterialApp(
+        home: BlocProvider<HomeBloc>(
+          create: (ctx) => HomeBloc(),
+          child: NewsList(),
+        ),
       ),
     );
   }
