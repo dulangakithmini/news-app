3a05efabf2816688b9e57a3a75e947354091a6ce
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-30 02:24:32 +0530

    Create a widget returning a RefreshIndicator to implement swipe to refresh

---
+++ lib/src/widgets/refresh.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+/// To implement swipe to refresh
+class Refresh extends StatelessWidget {
+  final Widget child;
+
+  Refresh({this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return RefreshIndicator(
+      child: child,
+      onRefresh: () {},
+    );
+  }
+}
