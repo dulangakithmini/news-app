43421ee1db7baefd0d970224ddc5703e838210eb
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-29 05:31:50 +0530

    Create ScanStreamTransformer

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -1,3 +1,4 @@
+import 'package:news/src/models/item_model.dart';
 import 'package:news/src/resources/repository.dart';
 import 'package:rxdart/rxdart.dart';
 
@@ -15,6 +16,18 @@ class StoriesBloc {
 
     /// Add to the PublishSubject
     _topIds.sink.add(ids);
+  }
+
+  _itemsTransformer() {
+    return ScanStreamTransformer(
+      (Map<int, Future<ItemModel>> cache, int id, int index) {
+        cache[id] = _repository.fetchItem(id);
+
+        /// The returned cache is carried over to the next time the function is invoked
+        return cache;
+      },
+      <int, Future<ItemModel>>{},
+    );
   }
 
   dispose() {
