a7478470ab7e82850a5e02d1749c64adf01b2752
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-29 17:31:35 +0530

    Implement StreamBuilder and FutureBuilder in NewsListTile

--- lib/src/widgets/news_list_tile.dart
+++ lib/src/widgets/news_list_tile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news/src/blocs/stories_provider.dart';
+import 'package:news/src/models/item_model.dart';
 
 class NewsListTile extends StatelessWidget {
   final int itemId;
@@ -11,6 +12,25 @@ class NewsListTile extends StatelessWidget {
     /// To access the StoriesBloc inorder to access StreamControllers inside StoriesBloc
     final bloc = StoriesProvider.of(context);
 
-    return Container();
+    return StreamBuilder(
+      stream: bloc.items,
+      builder: (context, AsyncSnapshot<Map<int, Future<ItemModel>>> snapshot) {
+        if (!snapshot.hasData) {
+          return Text('Still loading');
+        }
+        return FutureBuilder(
+          future: snapshot.data[itemId],
+
+          /// snapshot is named as itemSnapshot just to distinguish from the snapshot of the StreamBuilder
+          /// return type of itemSnapshot is not Future<ItemModel>, because we have already resolved Future with FutureBuilder
+          builder: (context, AsyncSnapshot<ItemModel> itemSnapshot) {
+            if (!itemSnapshot.hasData) {
+              return Text('Still loading item $itemId');
+            }
+            return Text(itemSnapshot.data.title);
+          },
+        );
+      },
+    );
   }
 }
