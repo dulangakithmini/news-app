03d0af4d3a46e32028757dd9e669199f535a597e
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-07-01 00:43:40 +0530

    Create NewsDetail UI

--- lib/src/screens/news_detail.dart
+++ lib/src/screens/news_detail.dart
@@ -3,8 +3,10 @@ import 'package:flutter/material.dart';
 class NewsDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('I\'m a News Detail'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Details'),
+      ),
     );
   }
 }
