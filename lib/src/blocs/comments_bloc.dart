9a5b871a222700290250438b69ac865df44261a2
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-07-01 02:12:28 +0530

    Create StreamControllers in commentsBloc and create  getters to streams and sinks

--- lib/src/blocs/comments_bloc.dart
+++ lib/src/blocs/comments_bloc.dart
@@ -1,1 +1,21 @@
-class CommentsBloc {}
+import 'package:news/src/models/item_model.dart';
+import 'package:rxdart/rxdart.dart';
+
+class CommentsBloc {
+  final PublishSubject<int> _commentsFetcher = PublishSubject<int>();
+  final BehaviorSubject<Map<int, Future<ItemModel>>> _commentsOutput =
+      BehaviorSubject<Map<int, Future<ItemModel>>>();
+
+  /// Getters to stream
+  Stream<Map<int, Future<ItemModel>>> get itemWithComments =>
+      _commentsOutput.stream;
+
+  /// Getters to sinks
+  void Function(int event) get fetchItemWithComments =>
+      _commentsFetcher.sink.add;
+
+  dispose() {
+    _commentsFetcher.close();
+    _commentsOutput.close();
+  }
+}
