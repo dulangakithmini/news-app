56c1b678a13e4fbdb601f2294ce4721601b31ec0
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-07-01 02:47:33 +0530

    Create a ScanStreamTransformer to transform comments

--- lib/src/blocs/comments_bloc.dart
+++ lib/src/blocs/comments_bloc.dart
@@ -1,12 +1,15 @@
 import 'dart:async';
 
 import 'package:news/src/models/item_model.dart';
+import 'package:news/src/resources/repository.dart';
 import 'package:rxdart/rxdart.dart';
 
 class CommentsBloc {
   final PublishSubject<int> _commentsFetcher = PublishSubject<int>();
   final BehaviorSubject<Map<int, Future<ItemModel>>> _commentsOutput =
       BehaviorSubject<Map<int, Future<ItemModel>>>();
+
+  final _repository = Repository();
 
   /// Getters to stream
   Stream<Map<int, Future<ItemModel>>> get itemWithComments =>
@@ -20,7 +23,25 @@ class CommentsBloc {
     _commentsFetcher.transform(_commentsTransformer()).pipe(_commentsOutput);
   }
 
-  StreamTransformer<int, dynamic> _commentsTransformer() {}
+  StreamTransformer<int, dynamic> _commentsTransformer() {
+    return ScanStreamTransformer<int, Map<int, Future<ItemModel>>>(
+      (Map<int, Future<ItemModel>> cache, int id, int index) {
+        /// Returns a future
+        cache[id] = _repository.fetchItem(id);
+
+        /// Above line returns a future.
+        /// Reference the Future and after the Future is resolved, the inner function will be invoked.
+        /// Inner function is fetched with the ItemModel just fetched.
+        cache[id].then((ItemModel item) {
+          /// Recursively get kids and fetch
+          item.kids.forEach((kidId) => fetchItemWithComments(kidId));
+        });
+
+        return cache;
+      },
+      <int, Future<ItemModel>>{},
+    );
+  }
 
   dispose() {
     _commentsFetcher.close();
