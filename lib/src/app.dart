77ec49077851e792dac339c7339fd8588d10a1c4
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-07-01 00:04:38 +0530

    Implement onGenerateRoute function to navigate to NewsDetail screen

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news/src/blocs/stories_provider.dart';
+import 'package:news/src/screens/home_screen/blocs/home_bloc.dart';
+import 'package:news/src/screens/news_detail.dart';
+
 // import 'package:news/src/screens/home_screen/home_screen.dart';
 import 'package:news/src/screens/news_list.dart';
 
@@ -9,18 +13,32 @@ class App extends StatelessWidget {
     return StoriesProvider(
       child: MaterialApp(
         /// Example - show NewsList page on the screen
-        onGenerateRoute: (RouteSettings settings) {
-          return MaterialPageRoute(
-            builder: (context) {
-              return NewsList();
-            },
-          );
-        },
-        // home: BlocProvider<HomeBloc>(
-        //   create: (ctx) => HomeBloc(),
-        //   child: NewsList(),
-        // ),
+        // onGenerateRoute: (RouteSettings settings) {
+        //   return MaterialPageRoute(
+        //     builder: (context) {
+        //       return NewsList();
+        //     },
+        //   );
+        // },
+        home: BlocProvider<HomeBloc>(
+          create: (ctx) => HomeBloc(),
+          child: NewsList(),
+        ),
+
+        /// When tap on a story, when pushNamed is called, onGenerateRoute is invoked.
+        onGenerateRoute: routes,
       ),
     );
+  }
+
+  Route routes(RouteSettings settings) {
+    /// Checks the route name and return the widget accordingly
+    if (settings.name != '/') {
+      return MaterialPageRoute(
+        builder: (context) {
+          return NewsDetail();
+        },
+      );
+    }
   }
 }
