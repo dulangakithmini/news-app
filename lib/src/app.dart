c38174374028affae0c4f68053e2df4bf50306b0
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-07-01 02:49:17 +0530

    Wrap the MaterialApp with CommentsProvider

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news/src/blocs/comments_provider.dart';
 import 'package:news/src/blocs/stories_provider.dart';
 import 'package:news/src/screens/home_screen/blocs/home_bloc.dart';
 import 'package:news/src/screens/news_detail.dart';
@@ -9,23 +10,25 @@ import 'package:news/src/screens/news_list.dart';
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StoriesProvider(
-      child: MaterialApp(
-        /// Example - show NewsList page on the screen
-        // onGenerateRoute: (RouteSettings settings) {
-        //   return MaterialPageRoute(
-        //     builder: (context) {
-        //       return NewsList();
-        //     },
-        //   );
-        // },
-        home: BlocProvider<HomeBloc>(
-          create: (ctx) => HomeBloc(),
-          child: NewsList(),
-        ),
+    return CommentsProvider(
+      child: StoriesProvider(
+        child: MaterialApp(
+          /// Example - show NewsList page on the screen
+          // onGenerateRoute: (RouteSettings settings) {
+          //   return MaterialPageRoute(
+          //     builder: (context) {
+          //       return NewsList();
+          //     },
+          //   );
+          // },
+          home: BlocProvider<HomeBloc>(
+            create: (ctx) => HomeBloc(),
+            child: NewsList(),
+          ),
 
-        /// When tap on a story, when pushNamed is called, onGenerateRoute is invoked.
-        onGenerateRoute: routes,
+          /// When tap on a story, when pushNamed is called, onGenerateRoute is invoked.
+          onGenerateRoute: routes,
+        ),
       ),
     );
   }
