1711d2003a6c81b046e7bede53a1a2a5cbfa8e3f
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-07-01 01:06:53 +0530

    Create a named constructor in NewsDetail

--- lib/src/screens/news_detail.dart
+++ lib/src/screens/news_detail.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
 class NewsDetail extends StatelessWidget {
+  final int itemId;
+
+  NewsDetail({this.itemId});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Details'),
       ),
+      body: Text('$itemId'),
     );
   }
 }
