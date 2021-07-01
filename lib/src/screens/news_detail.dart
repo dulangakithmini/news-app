60d82c09bb73e078c725be03ef4b94a05134f0d7
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-07-02 00:12:52 +0530

    Implement StreamBuilder and FutureBuilder fot NewsDetail

--- lib/src/screens/news_detail.dart
+++ lib/src/screens/news_detail.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:news/src/blocs/comments_bloc.dart';
+import 'package:news/src/blocs/comments_provider.dart';
+import 'package:news/src/models/item_model.dart';
 
 class NewsDetail extends StatelessWidget {
   final int itemId;
@@ -7,11 +10,35 @@ class NewsDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final bloc = CommentsProvider.of(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Details'),
       ),
-      body: Text('$itemId'),
+      body: buildBody(bloc),
+    );
+  }
+
+  Widget buildBody(CommentsBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.itemWithComments,
+      builder: (context, AsyncSnapshot<Map<int, Future<ItemModel>>> snapshot) {
+        if (!snapshot.hasData) {
+          return Text('Loading');
+        }
+        final itemFuture = snapshot.data[itemId];
+
+        return FutureBuilder(
+          future: itemFuture,
+          builder: (context, AsyncSnapshot<ItemModel> itemSnapshot) {
+            if (!itemSnapshot.hasData) {
+              return Text('Loading');
+            }
+            return Text(itemSnapshot.data.title);
+          },
+        );
+      },
     );
   }
 }
